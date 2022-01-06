aa90cfeb2b428ce6c3c6c9e024a57a20323d28fb
montblanc-github <86041660+montblanc-github@users.noreply.github.com>
2022-01-06 05:44:08 +0000

    :memo: 解説コメント追加（テキスト2周目のfizzbuzzファイル）
    テキスト2周目のfizzbuzzファイルに解説コメントを追加したため

--- second-lap/fizz_buzz_second.rb
+++ second-lap/fizz_buzz_second.rb
@@ -1,8 +1,12 @@
+# def以降のメソッド内に処理をまとめる
 def fizz_buzz(number)
+  # 3と5両方で割り切れる→15で割ったときの余りが0
   if number%15 == 0
     puts "FizzBuzz"
+  #3で割り切れる→3で割ったときの余りが0
   elsif number%3 == 0
     puts "Fizz"
+  # 5で割り切れる→5で割ったときの余りが0
   elsif number%5 == 0
     puts "Buzz"
   else
@@ -12,6 +16,7 @@ end
 
 puts "数字を入力してください"
 
+# 入力を受け取る
 input = gets.to_i
 
 puts "結果は..."
